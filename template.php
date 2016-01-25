bf9ca300cfeb61c47a7801001c30307784b7ccb6
Travis Lehman <travlehman@gmail.com>
2016-01-25 14:00:51 -0500

    file that shows intended markup

---
+++ template.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+	<!-- All CSS -->
+	<link rel="stylesheet" href="public/stylesheets/main.min.css">
+
+</head>
+<body data-spy="scroll" data-target="#legend">
+	<header id="global-header">
+		<h1>Destiny Scopes</h1>
+		<a class="jump-to-legend" href="#legend"><span>Legend</span></a>
+	</header>
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="primary-column col-md-9 col-lg-10">
+				<div id="iron-sights">
+				</div>
+				<div id="red-dot-sights">
+					<article class="item" id="quickdraw-is"><div class="visual"><img class="scope-ads" src="public/images/scopes/quickdraw-is-ads.jpg"><img class="scope-hip" src="public/images/scopes/quickdraw-is.jpg"><p class="zoom">Magnification: 2x</p><div class="details"><div class="icon" style="background-image: url(&quot;public/images/scope-icons/iron-sites.png&quot;);"></div><h2 class="name">QuickDraw IS</h2><p class="description">Agile sight. Lightning fastl handling.</p></div></div><ul class="stats"></ul></article>
+
+					<article class="item" id="truesight-is"><div class="visual"><img class="scope-ads" src="public/images/scopes/truesight-is-ads.jpg"><img class="scope-hip" src="public/images/scopes/truesight-is.jpg"><p class="zoom">Magnification: 2x</p><div class="details"><div class="icon" style="background-image: url(&quot;public/images/scope-icons/iron-sites.png&quot;);"></div><h2 class="name">TrueSight IS</h2><p class="description">Flexible sight. Light, with good recoil control. Strong target aquisition.</p></div></div><ul class="stats"></ul></article>
+
+					<article class="item" id="fastdraw-is"><div class="visual"><img class="scope-ads" src="public/images/scopes/fastdraw-is-ads.png"><img class="scope-hip" src="public/images/scopes/fastdraw-is.png"><p class="zoom">Magnification: 2x</p><div class="details"><div class="icon" style="background-image: url(&quot;public/images/scope-icons/iron-sites.png&quot;);"></div><h2 class="name">Fastdraw IS</h2><p class="description">Standard sight. A lightweight design, good for recoil control.</p></div></div><ul class="stats"></ul></article>
+
+				</div>
+				<div id="ranged-sights">
+				</div>
+				<div id="sniper-scopes">
+					
+					<article class="item" id="steadyhand-is"><div class="visual"><img class="scope-ads" src="public/images/scopes/steadyhand-is-ads.png"><img class="scope-hip" src="public/images/scopes/steadyhand-is.png"><p class="zoom">Magnification: 2x</p><div class="details"><div class="icon" style="background-image: url(&quot;public/images/scope-icons/iron-sites.png&quot;);"></div><h2 class="name">SteadyHand IS</h2><p class="description">Stable sight. Optimized for excellent recoil control.</p></div></div><ul class="stats"></ul></article>
+
+					<article class="item" id="sureshot-is"><div class="visual"><img class="scope-ads" src="public/images/scopes/sureshot-is-ads.jpg"><img class="scope-hip" src="public/images/scopes/sureshot-is.jpg"><p class="zoom">Magnification: 2x</p><div class="details"><div class="icon" style="background-image: url(&quot;public/images/scope-icons/iron-sites.png&quot;);"></div><h2 class="name">Sureshot IS</h2><p class="description">Snapshot sight. Excellent handling. Superb target aquisition.</p></div></div><ul class="stats"></ul></article>
+				</div>
+			</div>
+			<div class="secondary-column col-md-3 col-lg-2">
+				<div id="legend" class="legend" data-spy="affix" data-offset-top="30" data-offset-bottom="30">
+					<ul class="nav types">
+						<li class="nav-item">
+					        <a class="nav-link" href="#iron-sights">Iron Sights</a>
+						</li>
+					    <li class="nav-item">
+					        <a class="nav-link" href="#red-dot-scopes">Red Dot Sights</a>
+					        <ul class="nav scopes">
+					            <li class="nav-item active"><a class="nav-link" href="#quickdraw-is">QuickDrawIS</a></li>
+					            <li class="nav-item"><a class="nav-link" href="#truesight-is">TrueSightIS</a></li>
+					            <li class="nav-item"><a class="nav-link" href="#fastdraw-is">Fastdraw IS</a></li>
+					        </ul>
+					    </li>
+				    	<li class="nav-item">
+				            <a class="nav-link" href="#ranged-sights">Ranged Sights</a>
+				    	</li>
+					    <li class="nav-item">
+					        <a class="nav-link" href="#sniper-scopes">Sniper Scopes</a>
+					        <ul class="nav scopes">
+					            <li class="nav-item"><a class="nav-link" href="#steadyhand-is">SteadyHandIS</a></li>
+					            <li class="nav-item"><a class="nav-link" href="#sureshot-is">Sureshot IS</a></li>        
+					        </ul>
+					    </li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer id="global-footer">
+		<p>A hobby project by @tehtrav. Contact me on <a href="http://twitter.com/tehtrav">Twitter</a> or by
+		<script type="text/javascript" language="javascript">
+		// Email obfuscator script 2.1 by Tim Williams, University of Arizona
+		// Random encryption key feature by Andrew Moulden, Site Engineering Ltd
+		// This code is freeware provided these four comment lines remain intact
+		// A wizard to generate this code is at http://www.jottings.com/obfuscator/
+		{ coded = "nus95crr55nNzz@i6spr.IW6"
+			key = "fYXSRUcqhN1TLVMwisKy9EmrzPCtboIQ23Z74Ajxp5D0eB6uGJOnHWga8kvFdl"
+			shift=coded.length
+			link=""
+			for (i=0; i<coded.length; i++) {
+				if (key.indexOf(coded.charAt(i))==-1) {
+					ltr = coded.charAt(i)
+					link += (ltr)
+				}
+				else {
+					ltr = (key.indexOf(coded.charAt(i))-shift+key.length) % key.length
+					link += (key.charAt(ltr))
+				}
+			}
+			document.write("<a href='mailto:"+link+"'>Email</a>")
+		}
+		</script><noscript>Email</noscript> for questions or comments.</p>
+
+	</footer>
+
+	<script src="bower_components/jquery/dist/jquery.min.js"></script>
+	<script src="bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.min.js"></script>
+	<script src="resources/scripts/scopes.js"></script>
+
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-72752759-1', 'auto');
+		ga('send', 'pageview');
+	</script>
+</body>
+</html>
